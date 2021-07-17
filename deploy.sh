3236d13509fe58bd52b1d5feb9edab2b408955b8
mehmetafsar510 <drmehmet510@gmail.com>
2021-07-17 10:12:52 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /home/ec2-user/jenkins-dockerswarm/
-docker stack deploy --with-registry-auth -c <(docker-compose config) phonebook
+docker stack deploy --with-registry-auth -c <(docker-compose config) --resolve-image always phonebook
