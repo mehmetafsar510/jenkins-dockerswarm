f965c33733c1ac9cb4f1058ffc6b0cfbe887fc72
mehmetafsar510 <drmehmet510@gmail.com>
2021-07-17 09:05:54 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 cd /home/ec2-user/jenkins-dockerswarm/
-docker pull 646075469151.dkr.ecr.us-east-1.amazonaws.com/clarusway-repo/phonebook-app:latest
-docker build -t 646075469151.dkr.ecr.us-east-1.amazonaws.com/clarusway-repo/phonebook-app:latest .
-docker stack deploy -c <(docker-compose config) phonebook
+docker stack deploy --with-registry-auth -c <(docker-compose config) phonebook
