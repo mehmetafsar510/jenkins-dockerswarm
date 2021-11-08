ddbf3fd9ca1efa5c6b6c4f6adb3a31050dc5f3da
mehmetafsar510 <drmehmet510@gmail.com>
2021-11-08 15:02:51 +0300

    update

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 sh -c '''
-if [ -f "/home/ec2-user/{GIT_FOLDER}" ]
+if [ -f "{GIT_FOLDER}" ]
 then
-rm -rf /home/ec2-user/{GIT_FOLDER}
+rm -rf {GIT_FOLDER}
 git clone {GIT_URL}
 else
 git clone {GIT_URL}
